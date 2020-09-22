446b23046f114fee6fbfeb4444213481cdf106a5
user <https://github.com/yeongwoojang/ourGroup.git>
2020-09-22 16:07:21 +0900

    LoginActivity Push

--- app/src/main/java/com/stuty/studymatching/ACTIVITY/LoginActivity.java
+++ app/src/main/java/com/stuty/studymatching/ACTIVITY/LoginActivity.java
@@ -1,14 +1,16 @@
-package com.stuty.studymatching;
+package com.stuty.studymatching.ACTIVITY;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.stuty.studymatching.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
