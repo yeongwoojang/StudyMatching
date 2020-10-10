067a8ad960d15b761bb41bc9cf40c18df443eaf4
jgh6272 <jgh6272@naver.com>
2020-10-10 22:15:06 +0900

    User profile

--- app/src/main/java/com/stuty/studymatching/FRAGMENT/MainPage.java
+++ app/src/main/java/com/stuty/studymatching/FRAGMENT/MainPage.java
@@ -3,20 +3,31 @@ package com.stuty.studymatching.FRAGMENT;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
+import com.facebook.internal.metrics.Tag;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.stuty.studymatching.R;
 
 
@@ -30,6 +41,12 @@ public class MainPage extends Fragment {
     private ImageButton menuBt;
     private Toolbar toolbar;
     private NavigationView navigationView;
+
+    private CardView cardView;
+    private ImageView profileIV;
+    private EditText editText1,editText2,editText3,editText4;
+
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
     public MainPage newInstance(){return new MainPage();}
 
@@ -51,6 +68,32 @@ public class MainPage extends Fragment {
         mDrawerLayout = rootView.findViewById(R.id.drawer_layout);
         menuBt = rootView.findViewById(R.id.menu_button);
         navigationView = rootView.findViewById(R.id.nav_view);
+        editText1 = rootView.findViewById(R.id.nickNameET);
+        editText2 = rootView.findViewById(R.id.idET);
+        editText3 = rootView.findViewById(R.id.genderET);
+        editText4 = rootView.findViewById(R.id.majorET);
+        profileIV = rootView.findViewById(R.id.myface);
+        cardView = rootView.findViewById(R.id.cardview);
+
+        if (user != null) {
+            for(UserInfo profile : user.getProviderData()){
+                //로그인 제공 사이트
+                String providerId = profile.getProviderId();
+                //로그인 한 유저의 이름, 이메일, 프사
+                String name = user.getDisplayName();
+                String email = user.getEmail();
+                Uri photoUrl = user.getPhotoUrl();
+
+                //유저의 이메일 검증 체크
+                boolean emailVerified = user.isEmailVerified();
+
+                String uid = user.getUid();
+            }
+        }
+
+        editText1.setText(user.getDisplayName());
+        editText2.setText(user.getEmail());
+        profileIV.setImageURI(user.getPhotoUrl());
 
         return rootView;
     }
@@ -58,6 +101,13 @@ public class MainPage extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateUserProfile();
+            }
+        });
 
         menuBt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -102,4 +152,22 @@ public class MainPage extends Fragment {
         void successLogout();
     }
 
+
+    //프로필 수정
+    public void updateUserProfile(){
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setDisplayName("user")
+                .setPhotoUri(Uri.parse(""))
+                .build();
+        user.updateProfile(profileUpdates)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful())
+                        {
+                            Log.d("MainPage","User profile updated.");
+                        }
+                    }
+                });
+    }
 }
